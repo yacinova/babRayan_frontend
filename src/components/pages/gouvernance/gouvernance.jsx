57aa4b2aa652dea670b430c4177aac1a6da7eed6
mouad fanine <mouadfanine@gmail.com>
2024-11-22 17:32:03 +0100

    add member in gouvernance

--- src/components/pages/gouvernance/gouvernance.jsx
+++ src/components/pages/gouvernance/gouvernance.jsx
@@ -1,37 +1,77 @@
-import React, { useRef } from 'react';
-import { motion } from 'framer-motion';
-import './gouvernance.css';
- import CFI from '../../../assets/3rubrique/CFI.jpg';
-import pic1 from '../../../assets/gouvernance/gen.jpg';
-import pic2 from '../../../assets/gouvernance/pic3.jpg';
-import pic3 from '../../../assets/gouvernance/pic4.jpg';
+import React, { useRef } from "react";
+import { motion } from "framer-motion";
+import "./gouvernance.css";
+import CFI from "../../../assets/3rubrique/CFI.jpg";
+import pic1 from "../../../assets/gouvernance/gen.jpg";
+import pic2 from "../../../assets/gouvernance/pic3.jpg";
+import pic3 from "../../../assets/gouvernance/pic4.jpg";
+import me from "../../../assets/gouvernance/members/me.jpg";
+import profil from "../../../assets/gouvernance/members/profil.jpg";
+import profil2 from "../../../assets/gouvernance/members/profil2.jpg";
 
 const Gouvernance = () => {
   const ref = useRef(null);
 
+  const members = [
+    { name: 'Ratibe Hind', role: 'Membre Fondateur', position: 'Secrétaire général', avatar: profil2 },
+    { name: 'Hamroudi Fatima Zahra', role: 'Fondatrice-Présidente', avatar: profil },
+    { name: 'Ratibe Abdelmoula', role: 'Membre Fondateur', position: 'Vice-président', avatar: me },
+    { name: 'LALAMI Driss', role: 'Membre Fondateur', position: 'Trésorier Adjoint', avatar: me },
+    { name: 'Sefrioui Mouji Salim', role: 'Membre Fondateur', position: 'Trésorier général', avatar: profil2 },
+    { name: 'Jamal Lahjouji Mohamed', role: 'Membre Fondateur', position: 'Vice-président', avatar: profil },
+    { name: 'Anas Guennoun', role: 'Vice-président', avatar: profil },
+    { name: 'Mesbahi Zhor', position: 'Secrétaire Général Adjoint', avatar: me },
+    { name: 'Hamza Laghari', position: 'Vice-président', avatar: profil2 },
+  ];
+
   return (
     <div className="general-page">
       {/* Hero Section */}
-      <motion.div ref={ref} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}>
+      <motion.div
+        ref={ref}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
         <div className="hero-content">
-          <motion.div className="title" initial={{ y: 30, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ delay: 0.8, duration: 0.8 }}>
+          <motion.div
+            className="title"
+            initial={{ y: 30, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.8, duration: 0.8 }}
+          >
             <h1 className="hero-title">Gouvernance</h1>
             <div className="divider"></div>
-
           </motion.div>
         </div>
       </motion.div>
 
-      <div className='header'>
+      {/* <div className='header'>
           <p>
           Organigramme Général
           </p>
           <h2>
           Nous, engagés à titres divers et complémentaires dans l’association Bab Rayan pour l’aide à l’enfance
           </h2>
-      </div>
+      </div> */}
 
-      <div className="gouvernance-page"> 
+<div className="team-container">
+      <h1 className="team-title">Membres du bureau</h1>
+      <div className="team-grid">
+        {members.map((member, index) => (
+          <div className="team-member" key={index}>
+            <div className="avatar-placeholder">
+              <img src={member.avatar} alt={`${member.name}'s avatar`} />
+            </div>
+            <h3>{member.name}</h3>
+            <p>{member.role}</p>
+            <span>{member.position}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+
+      <div className="gouvernance-page">
         {/* Board of Directors Section */}
         <section className="section-container">
           <div className="image-text-wrapper">
@@ -41,7 +81,11 @@ const Gouvernance = () => {
             <div className="text-container">
               <h2>Conseil d'administration</h2>
               <p>
-                Le Conseil d'administration est l'instance décisionnaire. Il se réunit au moins quatre fois par an. Son rôle consiste à adopter le fonctionnement, déterminer la politique générale et suivre les orientations stratégiques. Il est composé de membres administrateurs.
+                Le Conseil d'administration est l'instance décisionnaire. Il se
+                réunit au moins quatre fois par an. Son rôle consiste à adopter
+                le fonctionnement, déterminer la politique générale et suivre
+                les orientations stratégiques. Il est composé de membres
+                administrateurs.
               </p>
             </div>
           </div>
@@ -56,7 +100,11 @@ const Gouvernance = () => {
             <div className="text-container">
               <h2>Direction Générale</h2>
               <p>
-                La Direction Générale gère les établissements et services sous l’autorité du Représentant de l'État. Elle assure une fonction stratégique dans la gestion des affaires, en lien avec le Conseil d'Administration et l’association, ainsi que le contexte économique, politique et scientifique.
+                La Direction Générale gère les établissements et services sous
+                l’autorité du Représentant de l'État. Elle assure une fonction
+                stratégique dans la gestion des affaires, en lien avec le
+                Conseil d'Administration et l’association, ainsi que le contexte
+                économique, politique et scientifique.
               </p>
             </div>
           </div>
@@ -70,14 +118,13 @@ const Gouvernance = () => {
             <div className="text-container">
               <h2>Partenaires institutionnels</h2>
               <p>
-              INDH (Initiative Nationale pour les Droits de l’Homme)
-Wilaya Grand Casablanca
-AREF Grand Casablanca (Académie Régional de l’Enseignement et de la Formation)
-Entraide Nationale Casa Anfa              </p>
+                INDH (Initiative Nationale pour les Droits de l’Homme) Wilaya
+                Grand Casablanca AREF Grand Casablanca (Académie Régional de
+                l’Enseignement et de la Formation) Entraide Nationale Casa Anfa{" "}
+              </p>
             </div>
           </div>
         </section>
-        
 
         {/* <h2> Transparence financière</h2> */}
 
